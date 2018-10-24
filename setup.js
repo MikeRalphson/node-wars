2d863766443b46fa5490369b5bcc5bf7d843fd09
Mike Ralphson <mike.ralphson@gmail.com>
2018-10-24 17:01:30 +0100

    setup; fix missing async callback

--- setup.js
+++ setup.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 
 // Set up dummy player
 var player = '{"name":"George","sector":1,"level":1,"credits":137837,"ship":"Imperial Starship","holds":100,"cargo":{"fuel":0,"organics":0,"equipment":0}}';
-fs.writeFile("data/player.json", player);
+fs.writeFile("data/player.json", player, function(err, result) {} );
 
 // Set up logs directory
 var mkdirSync = function (path) {
